cd01838af94189bf12d6db185a2b6d3bda559439
Derek Thurn <derek@fake.email>
2013-11-09 15:09:31 -0800

    move everything up one directory level

---
+++ Lagrange/JUnitCore.h
@@ -0,0 +1,14 @@
+//
+//  JUnitCore.h
+//  Lagrange
+//
+//  Created by Derek Thurn on 11/9/13.
+//
+//
+
+#ifndef Lagrange_JUnitCore_h
+#define Lagrange_JUnitCore_h
+
+#include "org/junit/runner/JUnitCore.h"
+
+#endif
